93e2d81343d2d3dbe5d94972c44ccca40f5e68bd
ccollins25 <56311983+ccollins25@users.noreply.github.com>
2019-11-30 23:33:35 -0500

    Update Song.cpp

--- Song.cpp
+++ Song.cpp
@@ -1,5 +1,73 @@
 #include "Song.h"
 
+//Default and Paramtrized Song Constructors
+Song::Song(){}
+Song::Song(std::string title, std::string artist, std::string album, int length, int year)
+{
+	this->title = title;
+	this->artist = artist;
+	this->album = album;
+	this->length = length;
+	this->year = year;
+}
+
+//Sets Song Details for Song Object
+void Song::set(std::string title, std::string artist, std::string album, int length, int year)
+{
+	this->title = title;
+	this->artist = artist;
+	this->album = album;
+	this->length = length;
+	this->year = year;
+}
+
+//Checks to see if two Song Objects are equal
+bool operator==(const Song& lhs, const Song& rhs)
+{
+	if ((lhs.title == rhs.title) && (lhs.artist == rhs.artist) && (lhs.album == rhs.album) &&
+		(lhs.length == rhs.length) && (lhs.year == rhs.year))
+		return true;
+	else 
+		return false;
+}
+
+//Overloaded String stream which prints Song Details to screen
+std::ostream& operator<<(std::ostream& os, const Song& song)
+{
+	os << song.title << "|" << song.artist << "|" << song.album << "|" << song.year << "|" << song.length;
+	return os;
+}
+
+//Overloaded String Stream which outputs Song Details to a file
+std::ofstream& operator<<(std::ofstream& os, const Song& song)
+{
+	os << "\n" << song.title << "," << song.artist << "," << song.album << "," << song.year << "," << song.length;
+	return os;
+}
+
+//Overloaded Output String Stream which reads in Song Details into Song Object
+std::istream& operator>>(std::istream& is, Song& song)
+{
+	char comma;
+	is >> song.title >> comma >> song.artist >> comma >> song.album >> comma >> song.year >> comma >> song.length;
+	
+	return is;
+}
+
+//Overloaded Output String Stream which reads in Song Details into a file
+std::fstream& operator>>(std::fstream& is, Song& song)
+{
+	
+	is >> song.title >> song.artist >>song.album >> song.year >> song.length;
+	return is;
+}
+
+int main(){
+	return 0;
+}
+
+/*#include "Song.h"
+
 #include <iostream>
 
 using namespace std;
@@ -80,4 +148,4 @@ bool Song::operator< (const Song& x) const {
 
 return (title_ < x.title_ );
 
-}
+}*/
