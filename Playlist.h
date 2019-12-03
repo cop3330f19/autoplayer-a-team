ae9bae3c864f98c7fb6bbbfddb12d6c891378b83
zharlan22 <54649096+zharlan22@users.noreply.github.com>
2019-12-03 17:19:23 -0500

    Update Playlist.h

--- Playlist.h
+++ Playlist.h
@@ -9,63 +9,57 @@
 #include <vector>
 
 class Playlist{
+    
+    private:
+    
+    int NumberOfSongs;
+	
 	public:
+    
 	//addSong function which adds Song Object to end of Playlist Object
 	Playlist addSong(Song& song) const;
+	Playlist operator+(Song& songAdd) const;
 	//deleteSong function which deletes all instances of user called Song Object from a specified playlist 
 	Playlist deleteSong(Song& song)const;
+	Playlist operator-(Song& songDelete)const;
 	//intersect function which returns all songs common in 
 	Playlist intersect(Playlist& list) const;
 	//Merge function which will return a new Playlist that combines all song objects from playlist called
 	//and those from playlist called by user
-	Playlist merge(Song& songList) const;
-	Playlist operator+(Song& listMerge) const;
+	Playlist merge(Playlist& list) const;
+	Playlist operator+(Playlist& listMerge) const;
 	//play function which prints message followed by title of song you are playing
 	//setMode function dictates how song is played
 	void play(Song& songPlay) const;
 	//setMode function which dictates next song Object to be played through 3 modes(normal,repeat,loop)
 	
-	private:
+    
+    int getNumberOfSongs()const;
+    void clearPlaylist();
+    void displayPlaylist(string);
+    
+    
 	
 };
 
 #endif
 /*#ifndef PLAYLIST_H
-
 #define PLAYLIST_H
-
 #include "Song.h"
-
 #include <set>
-
 #include <iostream>
-
 using namespace std;
-
 class PlayList{
-
 private:
-
 set<Song> playlist_;
-
 public:
-
 PlayList();
-
 PlayList(const Song& a_song);
-
 int getNumberOfSongs() const;
-
 bool isEmpty() const;
-
 bool addSong(const Song& new_song);
-
 bool removeSong(const Song& a_song);
-
 void clearPlayList();
-
 void displayPlayList() const;
-
 };
-
 #endif*/
