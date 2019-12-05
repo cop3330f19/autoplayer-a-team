a8de3b73029224dd633b6c0a4f7e46343fbd83ad
tmcmillan1014 <56973171+tmcmillan1014@users.noreply.github.com>
2019-12-05 10:36:17 -0500

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -1,60 +1,89 @@
-#include "Playlist.h"
-
+#include "playlist.h"
 #include <set>
-
 #include <iostream>
 
 using namespace std;
 
-Playlist::Playlist(){
+PlayList::PlayList(){
+    NumberOfSongs = 0;
+    SongsPlayed = 0;
+    IndexOfSongPlaying = 0;
+}
+
+PlayList::PlayList(const Song& a_song){
+    Song temp;
+    temp.album = a_song.album;
+    temp.year = a_song.year;
+    temp.artist = a_song.artist;
+    temp.title = a_song.title;
+    temp.length = a_song.length;
+    Songs.push_back(temp);
+    NumberOfSongs++;
+}
+
+int PlayList::getNumberOfSongs() const{
+
+return NumberOfSongs;
 
 }
 
-Playlist::Playlist(const Song& a_song){
+bool PlayList::isEmpty() const{
 
-playlist_.insert(a_song);
-
-}
-
-int Playlist::getNumberOfSongs()const{
-
-return playlist_.size();
+return ( NumberOfSongs == 0);
 
 }
 
-bool Playlist::isEmpty() const{
+bool PlayList::addSong(const Song& new_song){
 
-return ( playlist_.size() == 0);
+    Song temp;
+    temp.album = new_song.album;
+    temp.year = new_song.year;
+    temp.artist = new_song.artist;
+    temp.title = new_song.title;
+    temp.length = new_song.length;
+    Songs.push_back(temp);
+    NumberOfSongs++;
+    
+    return true;
+}
+
+vector<Song> getSongs() const{
+    return Songs;
+}
+
+
+
+bool PlayList::deleteSong(const Song& a_song){
+
+bool removed = false;
+    
+for (vector<Song>::iterator it=Songs.begin(); it!=Songs.end(); ++it)
+{
+    if((*it).getTitle() == a_song.getTitle() && (*it).getAuthor() == a_song.getAuthor() && (*it).getAlbum() == a_song.getAlbum())
+    {
+        Songs.erase(it);
+        NumberOfSongs--;
+        removed = true;
+    }
+}
+
+return removed;
 
 }
 
-bool Playlist::addSong(const Song& new_song){
+void PlayList::clearPlayList(){
 
-playlist_.insert(new_song);
-
-return true;
-
+Songs.clear();
+NumberOfSongs = 0;
+SongsPlayed = 0;
+IndexOfSongPlaying = 0;
 }
 
-bool Playlist::removeSong(const Song& a_song){
-
-playlist_.erase(a_song);
-
-return true;
-
-}
-
-void Playlist::clearPlaylist(){
-
-playlist_.clear();
-
-}
-
-void Playlist::displayPlaylist() const{
+void PlayList::displayPlayList() const{
 
 cout << "Playlist contains:" <<endl;
 
-for (set<Song>::iterator it=playlist_.begin(); it!=playlist_.end(); ++it)
+for (vector<Song>::iterator it= Songs.begin(); it!= Songs.end(); ++it)
 
 cout << "* Title" << (*it).getTitle() << " * Author" << (*it).getAuthor() << " * Album" << (*it).getAlbum() << " *" <<endl;
 
@@ -62,6 +91,46 @@ cout << "End of playlist" <<endl;
 
 }
 
+void PlayList::play() const
+{
+    if(SongsPlayed == 0)
+    {
+        cout << "Playing Song: " << Songs[0].getTitle() << endl;
+    }
+    else
+    {
+        if(Mode == "NORMAL")
+        {
+            if(IndexOfSongPlaying != (Songs.size() - 1))
+            {
+                IndexOfSongPlaying++;
+                cout << "Playing Song: " << Songs[IndexOfSongPlaying].getTitle() << endl;
+            }
+            else
+            {
+                IndexOfSongPlaying = 0;
+                cout << "Playing Song: " << Songs[IndexOfSongPlaying].getTitle() << endl;
+            }
+        }
+        else if(Mode == "REPEAT")
+        {
+            cout << "Playing Song: " << Songs[IndexOfSongPlaying].getTitle() << endl;
+        }
+        else if(Mode == "LOOP")
+        {
+            IndexOfSongPlaying = 0;
+            cout << "Playing Song: " << Songs[IndexOfSongPlaying].getTitle() << endl;
+        }
+    }
+    
+    SongsPlayed++;
+}
+
+
+void PlayList::setMode(string modesetter)
+{
+    Mode = modesetter;
+}
 /*#include "Playlist.h"
 #include <iostream>
 Playlist::Playlist(string name) {
