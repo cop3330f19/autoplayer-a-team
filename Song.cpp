5ddd14c9b59e197d7210de3d662ea31b23492f37
apugh3 <56900725+apugh3@users.noreply.github.com>
2019-12-04 22:17:51 -0500

    Update Song.cpp

--- Song.cpp
+++ Song.cpp
@@ -68,84 +68,21 @@ int main(){
 
 /*#include "Song.h"
 
-#include <iostream>
-
-using namespace std;
-
-//constructor
-
-Song::Song(){
-
-title_ = "" ;
-
-author_ = "";
-
-album_ = "";
-
+Song::Song(string songName, string firstLine){
+  this->songName = songName;
+  this->firstLine = firstLine;
+  timesPlayed = 0;
 }
 
-//paramerterized constructor
-
-Song::Song(const string& title, const string& author, const string& album){
-
-title_ = title ;
-
-author_ = author;
-
-album_ = album;
-
+string Song::GetName() const {
+  return songName;
 }
 
-//setter function
-
-void Song::setTitle(string title){
-
-title_ = title ;
-
+void Song::ListPrint() const {
+  cout << songName << ": \"" << firstLine << "\", " << timesPlayed << " play(s)." << endl;
 }
 
-void Song::setAuthor(string author){
-
-author_ = author;
-
+void Song::Play() {
+  cout << firstLine << endl;
+  timesPlayed++;
 }
-
-void Song::setAlbum(string album){
-
-album_ = album;
-
-}
-
-//gettter function
-
-string Song::getTitle() const{
-
-return title_;
-
-}
-
-string Song::getAuthor() const{
-
-return author_;
-
-}
-
-string Song::getAlbum() const{
-
-return album_;
-
-}
-
-bool operator==(const Song& lhs, const Song& rhs){
-
-return (lhs.title_ == rhs.title_ && lhs.author_ == rhs.author_ && lhs.author_ == rhs.author_);
-
-}
-
-//this function is mandatory for element insertion as iteratotr in set
-
-bool Song::operator< (const Song& x) const {
-
-return (title_ < x.title_ );
-
-}*/
